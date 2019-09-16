0d3c7fe2f28743a605fc295eb4ebf10bd16264c7
RobertWAccenture <robert.watkin@accenture.com>
2019-09-16 12:03:29 +0100

    updated library

--- src/weekthree/monday/library/Library.java
+++ src/weekthree/monday/library/Library.java
@@ -14,13 +14,51 @@ public class Library {
         books[3] = new Book(4,"Pride and Prejudice", "Jane Austen", "1813");
         books[4] = new Book(5,"The Canterbury Tales", "Geoffrey Chauer", "1387");
 
-        members[0] = new Member(1,"Dean","Lewis");
-        members[1] = new Member(2,"Steve","Beckwith");
-        members[2] = new Member(3,"Lisa", "Crow");
-        members[3] = new Member(4,"Robert", "Watkin");
-        members[4] = new Member(5, "Amanda", "Rae");
+        members[0] = new Member(1,"Dean","Lewis", (byte) 31);
+        members[1] = new Member(2,"Steve","Beckwith", (byte) 55);
+        members[2] = new Member(3,"Lisa", "Crow", (byte) 39);
+        members[3] = new Member(4,"Robert", "Watkin", (byte) 37);
+        members[4] = new Member(5, "Amanda", "Rae", (byte) 25);
 
         loans[0] = new Loan(2,4);
         loans[1] = new Loan(5,2);
+
+        menu();
+    }
+
+    private void menu(){
+        System.out.println("Welcome to Roberts library");
+        System.out.println("Please select an option from the list below");
+        System.out.println("1 - Search for a book now");
+        System.out.println("2 - Check if a book is available");
+        System.out.println("3 - Search for books by an author");
+        byte choice = Byte.parseByte(sc.nextLine());
+        handleChoice(choice);
+    }
+
+    private void handleChoice(byte choice) {
+        switch (choice){
+            case 1:
+                searchBook();
+                break;
+            case 2:
+                checkBook();
+                break;
+            case 3:
+                searchAuthor();
+                break;
+            default:
+                System.out.println("Invalid input provided, please try again");
+                menu();
+        }
+    }
+
+    private void searchAuthor() {
+    }
+
+    private void checkBook() {
+    }
+
+    private void searchBook() {
     }
 }
