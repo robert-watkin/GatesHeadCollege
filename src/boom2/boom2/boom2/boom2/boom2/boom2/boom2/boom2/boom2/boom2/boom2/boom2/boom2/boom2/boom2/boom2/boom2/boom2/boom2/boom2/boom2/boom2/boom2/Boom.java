1365c32455c99936ec2f133f1c08d3c1340cb36c
RobertWAccenture <robert.watkin@accenture.com>
2019-09-25 15:20:50 +0100

    completed code for report

--- src/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/Boom.java
+++ src/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/boom2/Boom.java
@@ -1,4 +1,9 @@
 package boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2.boom2;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Boom {
+    public static void main(String[] args) {
+        System.out.println("bitch");
+    }
 }
