c5567618c83adcdacc14d3de57ea4a8609935e1d
RobertWAccenture <robert.watkin@accenture.com>
2019-10-08 10:00:59 +0100

    challenges init

---
+++ src/weeksix/tuesday/challenges/MultiplicationTables.java
@@ -0,0 +1,20 @@
+/*
+Written By : Robert Watkin
+Date Created : 08/10/2019
+*/
+package weeksix.tuesday.challenges;
+
+import java.util.Scanner;
+
+public class MultiplicationTables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int startPoint = 0;
+        int endPoint = 0;
+        int mult = 0;
+        while (true) {
+            System.out.println("Start point : ");
+
+        }
+    }
+}
